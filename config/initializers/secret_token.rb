e4cbe4819b30e8f706b7b280f7b32c0114a31b0b
Nathaniel Barnes <nathaniel.barnes@activenetwork.com>
2013-11-03 11:21:29 -0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Activenow::Application.config.secret_key_base = 'b9096572f350b6b8bdeb0251076f79e447152c042f5b6ef7a77a45fb82c9e7514943ba77dba432dafc6d50e8c79ea2418495c0c58a4ea849cb19a676082c8d1d'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Activenow::Application.config.session_store :cookie_store, key: '_activenow_session'
