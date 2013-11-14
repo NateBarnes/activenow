cbd682fe3039c1597efeabd2695788c933d88216
Nathaniel Barnes <nathaniel.barnes@activenetwork.com>
2013-11-14 14:58:59 -0800

    Better be a fucking hash

--- config/initializers/google.rb
+++ config/initializers/google.rb
@@ -1,5 +1,5 @@
 file_contents = YAML.load(File.read("config/google_auth.yml")) rescue Hash.new
-GOOGLE_AUTH = file_contents[Rails.env]
+GOOGLE_AUTH = (file_contents||Hash.new)[Rails.env]
 
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :google_oauth2, (GOOGLE_AUTH[:key] or ENV["GOOGLE_KEY"]), (GOOGLE_AUTH[:secret] or ENV["GOOGLE_SECRET"])
