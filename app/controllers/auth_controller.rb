2bdaffd6c6421bb2e30887961a5636ff539175f1
Nathaniel Barnes <nathaniel.barnes@activenetwork.com>
2013-11-14 14:16:22 -0800

    More refresh_token nonsense

--- app/controllers/auth_controller.rb
+++ app/controllers/auth_controller.rb
@@ -8,7 +8,7 @@ class AuthController < ApplicationController
 
     if user
     else
-      User.create :name => request.env.fetch("omniauth.auth", {}).fetch("info", {}).fetch("name"), :refresh_token => refresh_token
+      User.create :name => request.env.fetch("omniauth.auth", {}).fetch("info", {}).fetch("name"), :glass_refresh_token => refresh_token
     end
 
     redirect_to '/'
