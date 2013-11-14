4a051675f27e9d7eb70536d8dcabc841835ec33f
Nathaniel Barnes <nathaniel.barnes@activenetwork.com>
2013-11-14 14:17:21 -0800

    Add new route

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Activenow::Application.routes.draw do
    root 'home#index'
    post 'auth/:provider/callback' => 'auth#callback'
    get 'auth/:provider/callback' => 'auth#callback'
+   get 'insert_event' => 'home#insert_event'
 end
