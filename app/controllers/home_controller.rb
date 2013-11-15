32695ce9f80f6ec35f73b558205ff70973459cbf
Nathaniel Barnes <nathaniel.barnes@activenetwork.com>
2013-11-15 14:14:00 -0800

    Read file for json of example event

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -6,7 +6,7 @@ class HomeController < ApplicationController
   end
 
   def insert_event
-    client.timeline.insert :text => "HELLO WORLD"
+    client.timeline.insert File.open("app/views/home/del_mar.json", "rb").read
     render :text => true
   end
 end
