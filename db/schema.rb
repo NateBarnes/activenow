214c35a5760b76d81561630e5c87b74c29f4a6ec
Nathaniel Barnes <nathaniel.barnes@activenetwork.com>
2013-11-19 14:28:12 -0800

    Add email to users

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131114184150) do
+ActiveRecord::Schema.define(version: 20131119222649) do
 
   create_table "users", force: true do |t|
     t.string   "name"
     t.string   "glass_refresh_token"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "email"
   end
 
 end
