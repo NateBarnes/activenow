6de58c31c6ca4aec1e73f190a7ccd5057c232035
Nathaniel Barnes <nathaniel.barnes@activenetwork.com>
2013-11-19 14:40:03 -0800

    Added notifications table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131119222649) do
+ActiveRecord::Schema.define(version: 20131119223945) do
+
+  create_table "notifications", force: true do |t|
+    t.integer  "user_id"
+    t.string   "asset_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: true do |t|
     t.string   "name"
