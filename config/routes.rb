4dd3a9fdfff73228df8bfed59fa9a75821be5681
Nathaniel Barnes <nathaniel.barnes@activenetwork.com>
2013-11-19 09:40:26 -0800

    I hear routes are important now...

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,6 @@ Activenow::Application.routes.draw do
    post 'auth/:provider/callback' => 'auth#callback'
    get 'auth/:provider/callback' => 'auth#callback'
    get 'insert_event' => 'home#insert_event'
+   get 'get_location' => 'home#get_location'
+   get 'find_local_event' => 'home#find_local_event'
 end
